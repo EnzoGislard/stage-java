f84e9fc1cab395c2dc081e6b64e2056664e06bb8
Unknown <antoine.casenove@viacesi.fr>
2019-09-23 13:36:56 +0200

    Mise en place de l'architecture du cryptage
    3 classes crées

---
+++ Décryptage/src/model/Map_Dic.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Map_Dic {
+	
+	public Map_Dic() {
+		
+	}		
+	
+	public String selectWord(String word) {
+		String noError = "No error with this returned";
+		
+		return noError;
+	}	
+}
