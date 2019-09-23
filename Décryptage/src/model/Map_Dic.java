7fa5cee87c20b0cb897199ec10be52d5ffd377fc
Unknown <antoine.casenove@viacesi.fr>
2019-09-23 14:35:03 +0200

    HashMap dictionary created

--- Décryptage/src/model/Map_Dic.java
+++ Décryptage/src/model/Map_Dic.java
@@ -1,9 +1,19 @@
 package model;
 
+import java.util.HashMap;
+
 public class Map_Dic {
+	
+	private String alphabet = "abcdefghijklmnopqrstuvwxyz";
+	public HashMap<String, String> dictionary = new HashMap<String, String>();	
 	
 	public Map_Dic() {
 		
+		for(int i = 0; i < 26; i++) {
+			char value = alphabet.charAt(i);
+			int key = i+97;
+			dictionary.put(Character.toString(value),  String.valueOf(key));
+		}
 	}		
 	
 	public String selectWord(String word) {
