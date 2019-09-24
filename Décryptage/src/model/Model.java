a2e09d2d20add00092c830aaa021840fa9028e83
hugoleboennec <hugo.leboennec@viacesi.fr>
2019-09-24 16:12:43 +0200

    Add file reader

--- Décryptage/src/model/Model.java
+++ Décryptage/src/model/Model.java
@@ -3,11 +3,15 @@ package model;
 
 public class Model{
 
-
+	ModelGestionFichier modelGestionFichier;
+	
 	public Model() {
 
-
+		modelGestionFichier = new ModelGestionFichier();
+		
 	}
+	
+	
 
 	
 }
