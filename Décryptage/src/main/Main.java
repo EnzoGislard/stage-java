edbc0c70ca7ea160a275153391d43e25e642270f
Unknown <antoine.casenove@viacesi.fr>
2019-09-24 17:57:30 +0200

    Amène les features BDD

--- Décryptage/src/main/Main.java
+++ Décryptage/src/main/Main.java
@@ -1,24 +1,38 @@
 package main;
 
+import java.sql.SQLException;
+
 import controller.Controller;
 import model.Model;
+import model.ModelCAD;
 import view.View;
 
 public abstract class Main {
 
-	@SuppressWarnings("unused")
-	public static void main(String[] args) {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
 
-		final Model model = new Model();
+        final Model model = new Model();
+
+        final Controller controller = new Controller(model);
+
+        final View view = new View(controller);
+
+        final ModelCAD Procedure = new ModelCAD();
 
 
-		final Controller controller = new Controller(model);
-		
-		
-		final View view = new View(controller);
+        controller.start();
 
-		controller.start();
+        Procedure.open();
 
-	}
+        try {
+            Procedure.getIdentifiant("enzo", "mdp");
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.print("Echec");
+        }
+
+    }
 
 }
--- Décryptage/src/model/ModelCAD.java
+++ Décryptage/src/model/ModelCAD.java
@@ -2,100 +2,104 @@ package model;
 
 import java.sql.*;
 
-
 public class ModelCAD {
 
-//	 DataSet GetRows(String rq_sql, String resultSetName) {
-//		 
-//		 
-//		 
-//		 
-//		 
-//	 }
+//     DataSet GetRows(String rq_sql, String resultSetName) {
+//
+//
+//
+//
+//
+//     }
 
-	private static String URL = "jdbc:mysql://localhost/test? autoReconnect=true&useSSL=false&useUnicode=true&useJDBC"
-			+ "CompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-	private static String LOGIN = "root";
-	private static String PASSWORD = "";
+    private static String URL = "jdbc:mysql://localhost/bddcryptage? autoReconnect=true&useSSL=false&useUnicode=true&useJDBC"
+            + "CompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static String LOGIN = "root";
+    private static String PASSWORD = "";
 
-	public Connection connection;
-	public Statement statement;
-	
-	
-	
-	
-	
+    public Connection connection;
+    public Statement statement;
 
-	public static String getMAPQuery(int id) {
-		return "call CALL_MAP(" + id + ");";
-	}
+    public static String getMAPQuery(int id) {
+        return "call CALL_MAP(" + id + ");";
+    }
 
-	
+    public static String getIdentifiantQuery(String Utilisateur, String Password) {
+        return "call Identification('" + Utilisateur + "', '" + Password + "');";
+    }
 
-	
-	public ModelCAD() {
+    public ModelCAD() {
 
-		this.connection = null;
-		this.statement = null;
+        this.connection = null;
+        this.statement = null;
 
-	}
+    }
 
-	public boolean open() {
+    public boolean open() {
 
-		System.out.println("\n\nopening a connection");
+        System.out.println("\n\nopening a connection");
 
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			this.connection = DriverManager.getConnection(ModelCAD.URL, ModelCAD.LOGIN, ModelCAD.PASSWORD);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager.getConnection(ModelCAD.URL, ModelCAD.LOGIN, ModelCAD.PASSWORD);
 
-			this.statement = this.connection.createStatement();
+            this.statement = this.connection.createStatement();
 
-		} catch (final ClassNotFoundException e) {
-			e.printStackTrace();
-			return false;
-		} catch (final SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
-	
-	
-	
-	
-	
-	
+        } catch (final ClassNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } catch (final SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 
-	public void close() {
+    public void close() {
 
-		System.out.println("closing a connection");
+        System.out.println("closing a connection");
 
-		if (connection != null)
-			try {
-				connection.close();
-			} catch (SQLException ignore) {
-			}
-	}
+        if (connection != null)
+            try {
+                connection.close();
+            } catch (SQLException ignore) {
+            }
+    }
 
-	public String getMAp(int id) throws SQLException {
+    public String getMAp(int id) throws SQLException {
 
-		final ResultSet resultSet = this.executeQuery(getMAPQuery(id));
+        final ResultSet resultSet = this.executeQuery(getMAPQuery(id));
+String map = "";
 
-		String map = "";
+        if (resultSet.first()) {
+            map = resultSet.getString("map");
+        }
 
-		if (resultSet.first()) {
-			map = resultSet.getString("map");
-		}
+        return map;
 
-		return map;
+    }
 
-	}
+    public String getIdentifiant(String Utilisateur, String Password) throws SQLException {
 
-	private ResultSet executeQuery(String query_p) throws SQLException {
+        ResultSet resultSet = this.executeQuery(getIdentifiantQuery(Utilisateur, Password));
 
-		ResultSet retur = this.statement.executeQuery(query_p);
+        if (resultSet.next()) {
+            System.out.println(resultSet.getString("Utilisateurs"));
+            System.out.println(resultSet.getString("Password"));
 
-		return retur;
+        } else {
+            System.out.println("Mauvais utilisateurs ou mauvais mot de passe");
+        }
 
-	}
+        return null;
+
+    }
+
+    private ResultSet executeQuery(String query_p) throws SQLException {
+
+        ResultSet retur = this.statement.executeQuery(query_p);
+
+        return retur;
+
+    }
 }
