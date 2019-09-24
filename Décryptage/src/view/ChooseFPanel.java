13fea83cdba157a1ace148d9f3252c60ce39e072
hugoleboennec <hugo.leboennec@viacesi.fr>
2019-09-24 10:21:11 +0200

    Add Button

--- Décryptage/src/view/ChooseFPanel.java
+++ Décryptage/src/view/ChooseFPanel.java
@@ -1,18 +1,43 @@
 package view;
 
+import java.awt.Color;
+
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class ChooseFPanel {
+public class ChooseFPanel extends JPanel {
 	
-    JFileChooser chooser = new JFileChooser();
-    FileNameExtensionFilter filter = new FileNameExtensionFilter(
-        "JPG & GIF Images", "jpg", "gif");
-    chooser.setFileFilter(filter);
-    int returnVal = chooser.showOpenDialog(parent);
-    if(returnVal == JFileChooser.APPROVE_OPTION) {
-       System.out.println("You chose to open this file: " +
-            chooser.getSelectedFile().getName());
-    }
+	private static final long serialVersionUID = 1L;
+	
+//    JFileChooser chooser = new JFileChooser();
+//    FileNameExtensionFilter filter = new FileNameExtensionFilter(
+//        "JPG & GIF Images", "jpg", "gif");
+//    chooser.setFileFilter(filter);
+//    int returnVal = chooser.showOpenDialog(parent);
+//    if(returnVal == JFileChooser.APPROVE_OPTION) {
+//       System.out.println("You chose to open this file: " +
+//            chooser.getSelectedFile().getName());
+//    }
+	
+	
+	// déclaration JP
+	JLabel TitleLabel;
+	JButton OpenButton;
+	
+	
+	// Propriétés attributs
 
+	TitleLabel = new JLabel("Choose File :", SwingConstants.CENTER);
+	TitleLabel.setBorder(lineborder);
+	TitleLabel.setForeground(Color.white);
+	
+	
+	
+	
+	
+	
 }
